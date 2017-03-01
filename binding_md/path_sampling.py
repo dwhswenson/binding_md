d1e516ddf294e7c60aa51f395920d89fa7e2674b
David W.H. Swenson <dwhs@hyperblazer.net>
2017-03-01 15:54:03 +0100

    Most of the draft of MultipleBindingEnsemble

--- binding_md/path_sampling.py
+++ binding_md/path_sampling.py
@@ -52,3 +52,83 @@ class StableContactsState(StorableNamedObject):
         else:
             return self.check_start(trajectory[-self.n_frames:])
 
+
+class MultipleBindingEnsemble(paths.Ensemble):
+    """
+    Ensemble for sampling unknown binding sites.
+
+    The stopping conditions (for both appending and prepending) are when the
+    path either reaches a known stable state, or has a set of contacts that
+    are stable for a fixed time period and during that time period is
+    *never* in a given excluded volume.
+
+    The definition of the contacts being stable is determined by the
+    :class:`.StableContactState` object.
+    """
+    def __init__(self, known_states, stable_contact_state,
+                 excluded_volume=None):
+        self.known_states = known_states
+        self.states = paths.join_volumes(known_states)
+        self.stable_contact_state = stable_contact_state
+        if excluded_volume is None:
+            excluded_volume = paths.EmptyVolume()
+        self.excluded_volume = excluded_volume
+        self.excluded_volume_ensemble = \
+                paths.AllOutXEnsemble(self.excluded_volume)
+
+    def _check_length(self, trajectory):
+        return len(trajectory) >= self.stable_contact_state.n_frames
+
+    def _window_edge(self, trajectory):
+        return (len(trajectory) % self.window_offset == 0)
+
+    def _should_check_stable_contacts(self, trajectory):
+        return (self._check_length(trajectory)
+                and self._window_edge(trajectory))
+
+    def _check_start(self, trajectory):
+        start = self.states(trajectory[0])
+        if not start and self._should_check_stable_contacts(trajectory):
+            start_subtraj = trajectory[0:self.stable_contact_state.n_frames]
+            start = (
+                self.excluded_volume_ensemble(start_subtraj)
+                and self.stable_contact_state.check_start(start_subtraj)
+            )
+        return start
+
+    def _check_end(self, trajectory):
+        end = self.states(trajectory[-1])
+        if not end and self._should_check_stable_contacts(trajectory):
+            end_subtraj = trajectory[-self.stable_contact_state.n_frames:]
+            end = (
+                self.excluded_volume_ensemble(end_subtraj)
+                and self.stable_contact_state.check_end(end_subtraj)
+            )
+        return end
+
+    def can_append(self, trajectory, trusted=None):
+        if trusted:
+            return self._check_end(trajectory)
+        else:
+            pass  # TODO
+
+    def can_prepend(self, trajectory, trusted=None):
+        if trusted:
+            return self._check_start(trajectory)
+        else:
+            pass  # TODO
+
+    def __call__(self, trajectory, trusted=None, candidate=False):
+        if candidate:
+            start = self._check_start(trajectory)
+            end = self._check_end(trajectory)
+            return start and end
+        else:
+            pass  # TODO
+
+    def strict_can_append(self, trajectory):
+        pass  # TODO
+
+    def strict_can_prepend(self, trajectory):
+        pass  # TODO
+

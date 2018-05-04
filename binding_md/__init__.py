76816d43e16510afb80ba144bf6dd49ecdab70dd
David W.H. Swenson <dwhs@hyperblazer.net>
2018-05-04 13:34:02 +0200

    Add SingleEnsembleNetwork to init

--- binding_md/__init__.py
+++ binding_md/__init__.py
@@ -2,6 +2,7 @@ from contact_analysis import pick_window, LigandContactAnalysis
 
 from path_sampling import (
     SingleEnsembleTransition, NetworkFromTransitions,  # TODO: move
+    SingleEnsembleNetwork,
     StableContactsState, MultipleBindingEnsemble,
     MultipleBindingShootingPointSelector,
     StableContactsCommittorSimulation

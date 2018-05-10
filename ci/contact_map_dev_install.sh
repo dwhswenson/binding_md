37c03a185a36bd7ba5b6665aecb8d233b63d1f46
David W.H. Swenson <dwhs@hyperblazer.net>
2018-05-10 12:19:38 +0200

    Add dev install of contact_map

---
+++ ci/contact_map_dev_install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This assumes you have already installed contact_map using conda
+pushd ~
+conda uninstall -y contact_map
+git clone https://github.com/dwhswenson/contact_map.git
+pushd contact_map
+pip install -e .
+
+if [ -z "$CONTACT_MAP_BRANCH" ]
+then
+    git checkout -b $CONTACT_MAP_BRANCH origin/$CONTACT_MAP_BRANCH
+fi
+git branch
+popd
+popd
